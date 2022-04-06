1feafc6d604d8fd1ba7cecab211edfc4551105e3
SON <son@SONs-Mac-mini.local>
2022-04-06 11:31:07 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'Widget/RestartApp.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    RestartApp(
+      child: Container(),
+    ),
+  );
+}
+
+
